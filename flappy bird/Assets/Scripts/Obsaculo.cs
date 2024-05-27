00e6b0cfcb00d72f141c137fc40bca9058371733
Thomas Sievers <thomas@qpix.com.br>
2024-05-27 11:24:13 -0300

    adicionando variedade de obstaculo

--- flappy bird/Assets/Scripts/Obsaculo.cs
+++ flappy bird/Assets/Scripts/Obsaculo.cs
@@ -7,6 +7,14 @@ public class Obsaculo : MonoBehaviour
     [SerializeField]
     private float velocidade = 0.6f;
 
+    [SerializeField]
+    private float variacaoPosicaoY;
+
+    private void Awake()
+    {
+        this.transform.Translate(Vector3.up * Random.Range(-variacaoPosicaoY, variacaoPosicaoY));
+    }
+
     void Update()
     {
         this.transform.Translate(Vector3.left * this.velocidade * Time.deltaTime);
