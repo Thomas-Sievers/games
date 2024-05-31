1bc6f0d29da74147ac15b8eb42e247f5999e8097
Thomas-Sievers <thomas@qpix.com.br>
2024-05-31 02:08:23 -0300

    adicionando sprites e musica

---
+++ flappy fish/Assets/Scripts/Obsaculo.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obsaculo : MonoBehaviour
+{
+    [SerializeField]
+    private float velocidade = 0.6f;
+
+    [SerializeField]
+    private float variacaoPosicaoY;
+
+    private void Awake()
+    {
+        this.transform.Translate(Vector3.up * Random.Range(-variacaoPosicaoY, variacaoPosicaoY));
+    }
+
+    void Update()
+    {
+        this.transform.Translate(Vector3.left * this.velocidade * Time.deltaTime);
+    }
+}
